1ba16516068f9c5d313e510c0da20e395553ea5e
Reichenbach, Michael <755327+Silthus@users.noreply.github.com>
2024-11-19 15:26:52 +0100

    fix: use pool name to create oidc provider

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "google_iam_workload_identity_pool_provider" "github" {
   display_name = var.id
   description  = "Workload Identity Provider for GitHub in ${var.github_organization}"
 
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github.id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.name
   workload_identity_pool_provider_id = "${var.id}-oidc"
 
   attribute_condition = "attribute.repository_owner == \"${var.github_organization}\""
